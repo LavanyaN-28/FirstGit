a76064f9fdfd71f9a411a6bf4e44713e780c968d
LavanyaN-28 <lavanya.g.n.777@gmail.com>
2023-11-30 16:23:39 +0530

    Added demo.js file

--- demo.js
+++ demo.js
@@ -1,1 +1,19 @@
-console.log("Hello!!")
+//console.log(document.URL)
+//console.log(document.title)
+//console.log(document.doctype)
+//console.log(document.head)
+//console.log(document.body)
+//console.log(document.forms)
+//console.log(document.links)
+//document.title="First-Program"
+
+
+/*var headerTitle=document.getElementById("header-title")
+console.log(headerTitle)
+var header=document.getElementById("main-header")
+header.style.borderBottom="solid 3px #000"
+*/
+
+var addItem=document.getElementById("item-title")
+addItem.style.color="green"
+addItem.style.fontWeight="bold"
