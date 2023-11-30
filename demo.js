be6b75e5497d3df8ecf57d2cae131390918dbf06
LavanyaN-28 <lavanya.g.n.777@gmail.com>
2023-11-30 17:30:00 +0530

    Updated the style of the list items

--- demo.js
+++ demo.js
@@ -14,6 +14,25 @@ var header=document.getElementById("main-header")
 header.style.borderBottom="solid 3px #000"
 */
 
-var addItem=document.getElementById("item-title")
+/*var addItem=document.getElementById("item-title")
 addItem.style.color="green"
+
 addItem.style.fontWeight="bold"
+*/
+var items=document.getElementsByClassName("list-group-item");
+console.log(items)
+console.log(items[1])
+/*items[1].textContent="Hello 2"
+items[1].style.fontWeight="bold"
+items[1].style.backgroundColor="yellow"
+
+//items.style.backgroundColor="#f4f4f4";
+
+for(var i=0; i<items.length;i++){
+    items[i].style.backgroundColor="#f4f4f4"
+}
+*/
+items[2].style.backgroundColor="green"
+for(var i=0; i<items.length;i++){
+    items[i].style.fontWeight="bold"
+}
